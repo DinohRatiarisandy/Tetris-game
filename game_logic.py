828afe0ff00851cc5cd8827aff538b5993c535ef
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-06 08:53:59 +0300

    drop the random shape on the grid (update)

--- game_logic.py
+++ game_logic.py
@@ -13,15 +13,15 @@ class Tetris:
         self.curr_shape = self.pick_random_shape()
         self.next_shape = None
 
-    def show(self, grid):
+    def show(self):
         """Show the current grid"""
 
-        return '\n'.join(''.join(map(str, cell)) for cell in grid)
+        return '\n'.join(' '.join(map(str, cell)) for cell in self.grid)
 
     def pick_random_shape(self):
         """Pick one shape randomly"""
 
-        return self.rotate_shape(SHAPES[random.randint(1, 7)])
+        self.curr_shape = self.rotate_shape(SHAPES[random.randint(1, 7)])
 
     def rotate_shape(self, shape):
         """Change the orientation of the shape"""
@@ -38,11 +38,31 @@ class Tetris:
             shape = new_shape
 
         return shape
+    
+    def drop_shape(self, shape):
+        """Drop the random shape on random col"""
+
+        rand_col = random.randint(0, self.COL-3)
+
+        # if the top row is all 0
+        top = all(shape[0][j]==0 for j in range(3))
+
+        # if the first column is all 0, we begin the loop for 1
+        left = all(shape[j][0]==0 for j in range(3))
+
+        # if the last column is all 0, we we terminate at 1
+        right = all(shape[j][2]==0 for j in range(3))
+
+        for i in range(top, 3):
+            for j in range(left, 3-right):
+                self.grid[i-top][j+rand_col] = shape[i][j]
 
     def play(self):
         """Play the game on console"""
 
-        print(self.show(self.pick_random_shape()))
+        self.pick_random_shape()
+        self.drop_shape(self.curr_shape)
+        print(self.show())
 
 if __name__ == "__main__":
     tetris = Tetris()
