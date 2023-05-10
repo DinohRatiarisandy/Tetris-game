df5e266cb8ee0fc7eb931936943bdcc89482336b
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-10 18:31:09 +0300

    tetris's constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+"""This module contains all constant's game"""
+
+COLORS = {
+    0: (26, 31, 40),   # dark grey
+    1: (116, 27, 124), # purple
+    2: (240, 227, 30), # yellow
+    3: (54, 126, 127), # baby blue
+    4: (55, 125, 34),  # green 
+    5: (201, 69, 58),  # orange 
+    6: (0, 32, 224),   # red
+    7: (128, 128, 128) # grey
+}
