276af86402c256bd722c6b8501fa30ddef3baa46
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-10 21:25:09 +0300

    create and manipulate the state of tetrominos

--- tetromino.py
+++ tetromino.py
@@ -4,19 +4,29 @@
 
 
 import constants
+import pygame
 
 class Block:
     """Class represent one block"""
 
     def __init__(self, id):
-        """id: categorie of the tetromino (L, I, J, O, T, S ou """
+        """id: categorie of the tetromino (L, I, J, O, T, S)"""
 
+        self.id = id
         self.colors = constants.COLORS
         self.cells = dict()
         self.CELL_SIZE = 30
         self.rotation = 0
     
-    def draw(self):
+    def draw(self, SCREEN):
         """Draw the block with his main color"""
 
-        
+        tiles = self.cells[self.rotation]
+        for tile in tiles:
+            tile_rect = pygame.Rect(
+                tile.column*self.CELL_SIZE+1,
+                tile.row*self.CELL_SIZE+1,
+                self.CELL_SIZE-1,
+                self.CELL_SIZE-1
+            )
+            pygame.draw.rect(SCREEN, self.colors[self.id], tile_rect)
---
+++ tetrominos.py
@@ -0,0 +1,20 @@
+"""
+    This module contains all tetrominoes and
+    all his rotation state
+"""
+
+
+from block import Block
+from pygame.math import Vector2
+
+class LBlock(Block):
+    """The tetromino L shape"""
+
+    def __init(self):
+        super().__init__(id=1)
+        self.cells = {
+            0: [Vector2(0, 2), Vector2(1, 0), Vector2(1, 1), Vector2(1, 2)],
+            1: [Vector2(0, 1), Vector2(1, 1), Vector2(2, 1), Vector2(2, 2)],
+            2: [Vector2(1, 0), Vector2(1, 1), Vector2(1, 2), Vector2(2, 0)],
+            3: [Vector2(0, 0), Vector2(0, 1), Vector2(1, 1), Vector2(2, 1)]
+        }
