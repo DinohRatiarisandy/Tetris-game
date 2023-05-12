21b0a3661449d1bd509865680f4b29c1037ed15a
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2023-05-12 20:54:48 +0300

    Game constants

--- constants.py
+++ constants.py
@@ -1,12 +1,14 @@
-"""This module contains all constant's game"""
+"""
+    This module contains all constant's game
+"""
 
 COLORS = {
-    0: (26, 31, 40),   # dark grey
-    1: (116, 27, 124), # purple
-    2: (240, 227, 30), # yellow
-    3: (54, 126, 127), # baby blue
-    4: (55, 125, 34),  # green 
-    5: (201, 69, 58),  # orange 
-    6: (0, 32, 224),   # red
-    7: (128, 128, 128) # grey
+    0: (26, 31, 40),     # dark grey
+    1: (116, 27, 124),   # purple
+    2: (240, 227, 30),   # yellow
+    3: (54, 126, 127),   # baby blue
+    4: (55, 125, 34),    # green 
+    5: (201, 69, 58),    # orange 
+    6: (128, 128, 128),  # grey
+    7: (240, 160, 184)   # rose saumon
 }
